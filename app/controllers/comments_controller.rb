ebb3a6d3daad3196d7160e7156c828b34188ba0b
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-12 17:46:30 +0900

    initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @prototype = Prototype.find(params[:prototype_id])
+    if @comment.save
+      redirect_to prototype_path(@prototype)
+    else
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
